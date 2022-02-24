8b11cfeba6719510b30031ea68f2301101126790
LENOVO <mdzubeir2468@gmail.com>
2022-02-24 23:38:54 +0530

    day15 CompareTo

---
+++ Day15/src/day15/CompareTo.java
@@ -0,0 +1,15 @@
+package day15;
+
+public class CompareTo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String x ="m" ;
+      String y ="z" ;
+      System.out.println("Result :" + x.compareTo(y));
+      Integer a = 1;
+      Integer b = 2;
+      System.out.println("Result :" + a.compareTo(b));
+	}
+
+}
